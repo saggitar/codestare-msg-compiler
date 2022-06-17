aac5db8290113e0666bae38fe9246199ea6dd568
Maximilian Schmidt <landlaeufer@gmail.com>
2022-06-17 17:13:42 +0200

    Make more verbose and add comments

--- src/codestare/compiletools/__init__.py
+++ src/codestare/compiletools/__init__.py
@@ -1,8 +1,20 @@
 import importlib
+import pathlib
+import typing
+
 from itertools import chain
 
 
-def has_module(*modules):
+def has_module(*modules: str):
+    """
+    Try importing module, catch ImportError
+
+    Args:
+        *modules: module names
+
+    Returns: True if module is importable else False
+    """
+
     try:
         for name in modules:
             importlib.import_module(name)
@@ -11,5 +23,19 @@ def has_module(*modules):
     return True
 
 
-def find_proto_files(*paths, recursive=True):
-    return list(dict.fromkeys(chain(*[p.glob(f"{'**/' if recursive else ''}*.proto") for p in paths])))
+def find_proto_files(*paths: pathlib.Path, recursive: bool = True) -> typing.List:
+    """
+    Return relative paths of .proto files in paths
+
+    Args:
+        *paths: We :func:`~pathlib.Path.glob` the files in the paths
+        recursive: check recursively in all paths if True
+
+    Returns:
+       List of unique paths of found .proto files, relative to search path
+    """
+    searcher = chain.from_iterable(
+        p.glob(f"{'**/' if recursive else ''}*.proto") for p in paths
+    )
+    found = list(dict.fromkeys(searcher))
+    return found
